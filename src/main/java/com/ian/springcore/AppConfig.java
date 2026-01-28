e6b8050039ca7907de4d672e23626d9178228454
XXN <jaechan27.0.2.28@gmail.com>
2026-01-28 21:29:09 +0900

    39. @Configuration과 바이트 코드 조작의 마법

--- src/main/java/com/ian/springcore/AppConfig.java
+++ src/main/java/com/ian/springcore/AppConfig.java
@@ -13,8 +13,20 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * 각 객체의 관심사를 분리하고 애플리케이션의 전체 동작 방식을 구성하기 위해 구현 객체를 생성하고 연결하는 설정 클래스
+ * <p>
+ * @Configuration: 해당 애너테이션이 붙은 config 파일을 CGLIB로 복제하여 스프링 빈으로 등록
+ * 스프링에서 CGLIB 라는 바이트 코드 조작 라이브러리를 사용하여 AppConfig 클래스를 상속받은 임의의 다른 클래스를 생성하고,
+ * 새로 생성한 클래스를 스프링 빈으로 등록 => 해당 클래스가 싱글톤이 보장되도록 해줌
+ * <p>
+ * CGLIB 예상 코드
+ * if (memoryMemberRepository가 이미 스프링 컨테이너에 등록되어 있으면?) {
+ *     return 스프링 컨테이너에서 찾아서 반환;
+ * } else { //스프링 컨테이너에 없으면
+ *     기존 로직을 호출해서 MemoryMemberRepository를 생성하고 스프링 컨테이너에 등록
+ *     return 반환
+ * }
  */
-@Configuration
+@Configuration // 즉, Configuration을 붙이지 않으면 CGLIB로 복제한 config 파일이 아닌 실제 config 파일이 빈으로 등록되기 때문에 싱글톤 보장 X
 public class AppConfig {
 
     /**
--- src/test/java/com/ian/springcore/singleton/ConfigurationSingletonTest.java
+++ src/test/java/com/ian/springcore/singleton/ConfigurationSingletonTest.java
@@ -28,4 +28,26 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberRepository1).isSameAs(memberRepository);
         Assertions.assertThat(memberRepository2).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        /**
+         * @Configuration: 해당 애너테이션이 붙은 config 파일을 CGLIB로 복제하여 스프링 빈으로 등록
+         * 스프링에서 CGLIB 라는 바이트 코드 조작 라이브러리를 사용하여 AppConfig 클래스를 상속받은 임의의 다른 클래스를 생성하고,
+         * 새로 생성한 클래스를 스프링 빈으로 등록 => 해당 클래스가 싱글톤이 보장되도록 해줌
+         * <p>
+         * CGLIB 예상 코드
+         * if (memoryMemberRepository가 이미 스프링 컨테이너에 등록되어 있으면?) {
+         *     return 스프링 컨테이너에서 찾아서 반환;
+         * } else { //스프링 컨테이너에 없으면
+         *     기존 로직을 호출해서 MemoryMemberRepository를 생성하고 스프링 컨테이너에 등록
+         *     return 반환
+         * }
+         */
+        System.out.println("bean = " + bean.getClass()); // bean = class com.ian.springcore.AppConfig$$SpringCGLIB$$0
+    }
 }
