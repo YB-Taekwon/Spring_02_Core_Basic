8413ccb98d5e6aac7b8cb46478abb50cd8742475
XXN <jaechan27.0.2.28@gmail.com>
2026-01-28 18:46:53 +0900

    34. 웹 애플리케이션과 싱글톤

---
+++ src/test/java/com/ian/springcore/singleton/SingletonTest.java
@@ -0,0 +1,29 @@
+package com.ian.springcore.singleton;
+
+import com.ian.springcore.AppConfig;
+import com.ian.springcore.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig config = new AppConfig();
+
+        // 조회: 호출 할 때 마다 객체 생성
+        MemberService memberService1 = config.memberService();
+        MemberService memberService2 = config.memberService();
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
