eb532b550900a92ede49267d79387db1780fa4a5
XXN <jaechan27.0.2.28@gmail.com>
2026-01-29 17:08:08 +0900

    44. 다양한 의존 관계 주입 방법

--- src/main/java/com/ian/springcore/member/MemberServiceImpl.java
+++ src/main/java/com/ian/springcore/member/MemberServiceImpl.java
@@ -8,10 +8,19 @@ public class MemberServiceImpl implements MemberService {
 
     // config를 통한 생성자 주입 방식으로 변경
     //    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 의존성 주입 방식: 생성자 주입
+     * - 생성자 호출 시점에 딱 1번만 호출되는 것이 보장됨
+     * - 불변, 필수 의존 관계에 주로 사용
+     * 1. 사용할 필드 선언
+     * 2. 생성자 호출로 필드 값 초기화 및 객체 생성
+     * 3. 필드가 final이기 때문에 한 번 초기화 된 값을 변경할 수 없음
+     */
     private final MemberRepository memberRepository;
 
-    // Autowired: 의존 관계를 자동으로 주입 -> 기본적으로 타입이 동일한 빈을 찾아서 주입
-    @Autowired // ac.getBean(MemberRepository.class);
+    // Autowired: 의존 관계를 자동으로 주입, 생성자가 하나일 경우 생략 가능
+    @Autowired // 기본적으로 타입이 동일한 빈을 찾아서 주입 -> ac.getBean(MemberRepository.class);
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- src/main/java/com/ian/springcore/order/OrderServiceImpl.java
+++ src/main/java/com/ian/springcore/order/OrderServiceImpl.java
@@ -17,6 +17,15 @@ public class OrderServiceImpl implements OrderService {
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    /**
+     * 의존성 주입 방식: 생성자 주입
+     * - 생성자 호출 시점에 딱 1번만 호출되는 것이 보장됨
+     * - 불변, 필수 의존 관계에 주로 사용
+     * 1. 사용할 필드 선언
+     * 2. 생성자 호출로 필드 값 초기화 및 객체 생성
+     * 3. 필드가 final이기 때문에 한 번 초기화 된 값을 변경할 수 없음
+     */
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
@@ -38,4 +47,38 @@ public class OrderServiceImpl implements OrderService {
     public MemberRepository getMemberRepository() {
         return memberRepository;
     }
+
+    /**
+     * 의존성 주입 방식: setter 주입
+     * - 선택, 변경 가능성이 있는 의존 관계에서 사용
+     * 1. 사용할 필드 선언
+     * 2. 생성자 호출로 필드의 빈 객체 생성
+     * 3. setter로 의존 관계 주입
+     */
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+//
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    /**
+     * 의존성 주입 방식: 필드 주입
+     * - 의존 관계가 주입되지 않은 상태로 객체가 만들어지기 때문에 기본 생성자 호출 시 NPE 발생 가능성이 높음
+     * - 외부에서 필드에 직접 의존 관계를 주입할 수 있는 방법이 없음 -> 외부에서 의존 관계 주입 시 setter를 사용해야 함
+     * 1. 사용할 필드 선언
+     * 2. 생성자 호출로 필드의 빈 객체 생성
+     * 3. 사용할 의존성 주입
+     */
+//    @Autowired
+//    private MemberRepository memberRepository;
+//
+//    @Autowired
+//    private DiscountPolicy discountPolicy;
 }
