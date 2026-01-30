d84b2129d19318acc89b4f7d471608dd344817ea
XXN <jaechan27.0.2.28@gmail.com>
2026-01-30 18:23:09 +0900

    60. 프로토타입 스코프 - 싱글톤 빈과 함께 사용 시 Provider로 문제 해결

--- src/test/java/com/ian/springcore/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/com/ian/springcore/scope/SingletonWithPrototypeTest1.java
@@ -2,7 +2,11 @@ package com.ian.springcore.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -47,7 +51,7 @@ public class SingletonWithPrototypeTest1 {
         ClientBean clientBean2 = context.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
 
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("prototype")
@@ -77,15 +81,41 @@ public class SingletonWithPrototypeTest1 {
     @Scope("singleton")
     static class ClientBean {
 
-        private final PrototypeBean prototypeBean;
+//        private final PrototypeBean prototypeBean;
+//
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        /**
+         * 프로바이더에서 스프링 컨테이너에서 특정 의존성을 찾아주는 기능(DL: Dependency Lookup, 의존성 탐색/조회)만 따로 제공
+         * -> 애플리케이션 컨텍스트가 빈을 직접 찾지 않음 (애플리케이션 컨텍스트 대신 조회해줌)
+         * - ObjectFactory
+         * - ObjectProvider
+         * 단, 스프링에 의존적임
+         */
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        /**
+         * 자바 표준 프로바이더 -> build.gradle에 라이브러리 추가
+         * 강의에서 사용한 javax.inject의 경우 Spring 6에서 jakarta.inject로 변경됨
+         */
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            prototypeBean.addCount();
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
 
+            // 프로바이더에서 getObject()를 호출하면 그제서야 스프링 컨테이너에서 프로토타입 빈을 찾아서 반환
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+
+            // 자바 표준 프로바이더
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            prototypeBean.addCount();
             return prototypeBean.getCount();
         }
     }
