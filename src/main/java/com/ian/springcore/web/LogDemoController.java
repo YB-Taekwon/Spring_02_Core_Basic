dd8eb07c29a076a60eae59ae776579869bc0218f
XXN <jaechan27.0.2.28@gmail.com>
2026-01-30 19:53:10 +0900

    63. 스코프와 Provider

--- src/main/java/com/ian/springcore/web/LogDemoController.java
+++ src/main/java/com/ian/springcore/web/LogDemoController.java
@@ -3,6 +3,8 @@ package com.ian.springcore.web;
 import com.ian.springcore.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
+    /**
+     * Provider 사용 전
+     * 싱글톤 컨테이너가 빈을 생성할 때 요청 정보가 없어서 MyLogger 빈(scope=request)을 생성할 수 없어서 예외 발생
+     * Provider 사용 후
+     * 빈 조회 시점을 실제 요청 처리 시점으로 늦춰서 빈이 정상적으로 생성 및 초기화가 됨
+     */
+    @GetMapping
     public String logDemo(HttpServletRequest request) {
-        myLogger.setRequestUrl(request.getRequestURL().toString());
+        String requestUrl = request.getRequestURL().toString();
+
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestUrl(requestUrl);
         myLogger.log("Controller Test");
 
         logDemoService.logic("testId");
 
         return "OK";
     }
-
 }
--- src/main/java/com/ian/springcore/web/LogDemoService.java
+++ src/main/java/com/ian/springcore/web/LogDemoService.java
@@ -2,15 +2,17 @@ package com.ian.springcore.web;
 
 import com.ian.springcore.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("Service Id = " + id);
     }
 }
